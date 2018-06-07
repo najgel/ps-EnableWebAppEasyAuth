d893345fff67422547232c1fee42f97fad0967b4
Niklas Gustafsson <gustafsson.niklas@gmail.com>
2018-06-07 23:35:18 +0200

    First commit

---
+++ EnableEasyAuth.ps1
@@ -0,0 +1,38 @@
+## Heavily inspired by https://blogs.msdn.microsoft.com/azuregov/2017/12/06/web-app-easy-auth-configuration-using-powershell/ Thanks!!!
+
+
+param(
+ 
+    # Parameter help description
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$ResourceGroupName,
+ 
+    [Parameter(Mandatory=$true,Position=2)]
+    [string]$WebAppName,
+ 
+    #Issuer is the aad
+    [Parameter(Mandatory=$true,Position=4)]
+    [string]$IssuerUrl
+ 
+)
+
+$AppID = (Get-AzureRmADApplication -DisplayName $WebAppName).ApplicationId.Guid
+
+if([string]::IsNullOrEmpty($AppID)){
+    New-AzureRmADApplication -DisplayName $WebAppName -IdentifierUris "https://$WebAppName.azurewebsites.net"
+    $global:AppID = (Get-AzureRmADApplication -DisplayName $WebAppName).ApplicationId.Guid
+}
+
+$authResourceName = $WebAppName + "/authsettings"
+
+$auth = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $authResourceName -Action list -ApiVersion 2016-08-01 -Force
+ 
+$auth.properties.enabled = "True"
+$auth.properties.unauthenticatedClientAction = "RedirectToLoginPage"
+$auth.properties.tokenStoreEnabled = "True"
+$auth.properties.defaultProvider = "AzureActiveDirectory"
+$auth.properties.isAadAutoProvisioned = "False"
+$auth.properties.clientId = $ClientId
+$auth.properties.issuer = $IssuerUrl
+
+New-AzureRmResource -PropertyObject $auth.properties -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $authResourceName -ApiVersion 2016-08-01 -Force
